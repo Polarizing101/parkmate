b75e2624b6ef6e438a818d21b83136ec9b8435ba
Panagiotis Kassanos <panosk-8789@hotmail.com>
2026-08-14 16:58:37 +0300

    Create Spring Boot project structure

---
+++ backend/parkmate/src/main/java/com/parkmate/ParkmateApplication.java
@@ -0,0 +1,13 @@
+package com.parkmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkmateApplication.class, args);
+	}
+
+}
---
+++ backend/parkmate/src/test/java/com/parkmate/ParkmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
