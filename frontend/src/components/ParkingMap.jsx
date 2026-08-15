2b75c283a6f5c0605fbefca0229899ed4c468bc0
Panagiotis Kassanos <panosk-8789@hotmail.com>
2026-08-15 17:50:36 +0300

    Display parking spots on map

--- frontend/src/components/ParkingMap.jsx
+++ frontend/src/components/ParkingMap.jsx
@@ -1,7 +1,41 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup
+} from "react-leaflet";
+
 import "leaflet/dist/leaflet.css";
 
+import { useEffect, useState } from "react";
+
+import { getParkingSpots } from "../services/parkingService";
+
 function ParkingMap() {
+
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+
+    loadParkingSpots();
+
+  }, []);
+
+  const loadParkingSpots = async () => {
+
+    try {
+
+      const response =
+        await getParkingSpots();
+
+      setSpots(response.data);
+
+    } catch (error) {
+
+      console.error(error);
+    }
+  };
+
   return (
     <MapContainer
       center={[37.9838, 23.7275]}
@@ -16,6 +50,34 @@ function ParkingMap() {
         attribution="OpenStreetMap"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      {spots.map((spot) => (
+
+        <Marker
+          key={spot.id}
+          position={[
+            spot.latitude,
+            spot.longitude
+          ]}
+        >
+
+          <Popup>
+
+            <strong>
+              {spot.street}
+            </strong>
+
+            <br />
+
+            Status:
+            {" "}
+            {spot.status}
+
+          </Popup>
+
+        </Marker>
+
+      ))}
 
     </MapContainer>
   );
--- frontend/src/services/parkingService.js
+++ frontend/src/services/parkingService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/parking-spots";
+const API_URL =
+  "http://localhost:8080/api/parking-spots";
 
-export const getAllParkingSpots = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
+export const getParkingSpots = async () => {
+  return axios.get(API_URL);
 };
