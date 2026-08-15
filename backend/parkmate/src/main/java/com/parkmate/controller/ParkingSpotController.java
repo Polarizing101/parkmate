73ae370c47ae523f82cf8f83b5d5ff5e067bd35c
Panagiotis Kassanos <panosk-8789@hotmail.com>
2026-08-15 12:53:02 +0300

    Add parking spot REST endpoints

---
+++ backend/parkmate/src/main/java/com/parkmate/controller/ParkingSpotController.java
@@ -0,0 +1,28 @@
+package com.parkmate.controller;
+
+import com.parkmate.entity.ParkingSpot;
+import com.parkmate.service.ParkingSpotService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/parking-spots")
+public class ParkingSpotController {
+
+    private final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+
+    @GetMapping
+    public List<ParkingSpot> getAllParkingSpots() {
+        return parkingSpotService.getAllParkingSpots();
+    }
+
+    @PostMapping
+    public ParkingSpot createParkingSpot(@RequestBody ParkingSpot parkingSpot) {
+        return parkingSpotService.createParkingSpot(parkingSpot);
+    }
+}
